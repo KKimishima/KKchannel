ce16573b89d8dce92b562816e6dc06e3e3837587
君島克弥 <K.Kimishima123@gmail.com>
2018-03-03 14:14:09 +0900

    <追加>ログインフォームから受け取るコントローラ実装 refs #49
    loginPageクラスを削除してLoginクラスに集約

--- src/main/java/com/github/KKimishima/controller/Login.java
+++ src/main/java/com/github/KKimishima/controller/Login.java
@@ -1,6 +1,5 @@
 package com.github.KKimishima.controller;
 
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,15 +8,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/LoginPage")
-public class LoginPage extends HttpServlet{
+@WebServlet("/Login")
+public class Login extends HttpServlet{
   private final static long serialVersionUID = 1L;
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    RequestDispatcher rd = req.getRequestDispatcher(
-        "/WEB-INF/jsp/loginPage.jsp"
+        RequestDispatcher rd = req.getRequestDispatcher(
+        "/WEB-INF/jsp/Login.jsp"
     );
     rd.forward(req,resp);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+   req.setCharacterEncoding("UTF-8");
+   String user = req.getParameter("user");
+   String pass = req.getParameter("pass");
+
+   System.out.println("user =" + user +":  pass = " + pass);
   }
 }
