30a8b9f7bac95d677a13cfafcc6062de6dead9f7
君島克弥 <K.Kimishima123@gmail.com>
2018-03-03 15:23:49 +0900

    <追加>ログインユーザモデル作製 refs #45

---
+++ src/main/java/com/github/KKimishima/model/LoginUser.java
@@ -0,0 +1,23 @@
+package com.github.KKimishima.model;
+
+import com.github.KKimishima.controller.Login;
+
+import java.io.Serializable;
+
+public class LoginUser implements Serializable{
+  private String user;
+  private String pass;
+  public LoginUser(){}
+  public LoginUser(String user,String pass){
+    this.user = user;
+    this.pass = pass;
+  }
+
+  public String getPass() {
+    return pass;
+  }
+
+  public String getUser() {
+    return user;
+  }
+}
