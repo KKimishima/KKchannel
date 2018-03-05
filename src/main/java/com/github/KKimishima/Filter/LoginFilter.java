96524d9bad8188ec501ffb9e9110cfc5d0f9c6b5
君島克弥 <K.Kimishima123@gmail.com>
2018-03-05 21:52:36 +0900

    <追加>ログインフィルタとutf-8用フィルタ作製 refs #50

---
+++ src/main/java/com/github/KKimishima/Filter/LoginFilter.java
@@ -0,0 +1,36 @@
+package com.github.KKimishima.Filter;
+
+import com.github.KKimishima.model.LoginUser;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/Main/*")
+public class LoginFilter implements Filter {
+  @Override
+  public void init(FilterConfig filterConfig) {
+
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws ServletException, IOException {
+    HttpSession session = ((HttpServletRequest)request).getSession();
+    LoginUser loginUser = (LoginUser) session.getAttribute("loginUser");
+    if (loginUser == null){
+      session.removeAttribute("loginUser");
+      ((HttpServletResponse)response).sendRedirect("/KKchannel/Login");
+      return;
+    }
+    chain.doFilter(request,response);
+  }
+
+  @Override
+  public void destroy() {
+
+  }
+}
