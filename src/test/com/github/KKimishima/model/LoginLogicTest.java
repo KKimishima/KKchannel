57ca6205fbc23d8fce2516f1899239b4914c670b
君島克弥 <K.Kimishima123@gmail.com>
2018-03-09 21:22:13 +0900

    <変更>ログインロジック変更　refs #65
    nullで判別するように変更

--- src/test/com/github/KKimishima/model/LoginLogicTest.java
+++ src/test/com/github/KKimishima/model/LoginLogicTest.java
@@ -13,12 +13,14 @@ public class LoginLogicTest {
   public void executeTest成功のパスワードを渡して成功させる() {
     LoginUser loginUser = new LoginUser("admin","admin");
     LoginLogic loginLogic = new LoginLogic(loginUser);
-    assertThat(true,is(loginLogic.execute()));
+    assertThat(true,is(loginLogic.execute(loginUser)));
+    assertThat(1,is(loginUser.getUserID()));
   }
   @Test
   public void executeTest失敗のパスワードを渡して失敗させる() {
     LoginUser loginUser = new LoginUser("ほげ","ほげ");
     LoginLogic loginLogic = new LoginLogic(loginUser);
-    assertThat(false,is(loginLogic.execute()));
+    assertThat(false,is(loginLogic.execute(loginUser)));
+    assertThat(null,is(loginUser.getUserID()));
   }
 }
