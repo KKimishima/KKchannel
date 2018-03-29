877df5cc02c646d925d55dbf28cc911a8d42dd81
君島克弥 <K.Kimishima123@gmail.com>
2018-03-30 01:03:15 +0900

    <追加>httpメソッドのdeldeを実装 refs #71
    削除用のserlvet作製

--- src/main/java/com/github/KKimishima/controller/Delete.java
+++ src/main/java/com/github/KKimishima/controller/Delete.java
@@ -1,17 +1,34 @@
 package com.github.KKimishima.controller;
 
+import com.github.KKimishima.model.Logic.DeleteLogic;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Optional;
 
 @WebServlet("/Main/Delete")
 public class Delete extends HttpServlet{
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    String test = req.getParameter("postID");
-    System.out.println("delete = " + test);
+    String postID = req.getParameter("postID");
+    int postIDNum = Integer.parseInt(postID);
+    boolean check;
+
+    DeleteLogic deleteLogic = new DeleteLogic(postIDNum);
+    if (deleteLogic.execute()){
+      check = true;
+    }else{
+      check = false;
+    }
+
+    resp.setContentType("text/plain;charset=UTF-8");
+    PrintWriter pw = resp.getWriter();
+    pw.print(check);
+    pw.close();
   }
 }
